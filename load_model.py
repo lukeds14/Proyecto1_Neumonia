061213723e9896fb89258d795566936269214398
lukeds14 <lukeds14@gmail.com>
2024-07-23 14:59:57 -0500

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -3,7 +3,7 @@ tf.compat.v1.disable_eager_execution()
 tf.compat.v1.experimental.output_all_intermediates(True)
 
 def model_fun():
-    model = tf.keras.models.load_model('D:\Especializacion IA\Proyectos_IA\modulo1\conv_MLP_84.h5')
+    model = tf.keras.models.load_model('conv_MLP_84.h5')
     if not model.compiled_loss:
         loss = tf.keras.losses.CategoricalCrossentropy(reduction='sum_over_batch_size')
         model.compile(optimizer='adam', loss=loss)
